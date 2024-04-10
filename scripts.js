12d6b389916a4efb8ed8fd33de43bdc14fca86ab
fabiandrago2803 <fabiandragonetti@gmail.com>
2024-04-10 20:52:21 -0300

    otro

--- scripts.js
+++ scripts.js
@@ -1,39 +1,45 @@
-function validate()
+function validacion()
 {
-    var nombre = document.getElementById("nombre").value;
-    var apellido = document.getElementById("apellido").value;
-    var vehiculo = document.getElementById("vehiculo").checked;
-    var email = document.getElementById("E-mail").value;
-    //validacion de que el mail este completo
-alert(nombre + " " + apellido);
+    var  nombre = document.getElementById("nombre").value;
+    var  apellido = document.getElementById("apellido").value;
+    const vehiculo = document.querySelectorAll('input[name="vehiculo"]');
+    var email = document.getElementById( "email" ).value;
+    var resultado;
+    console.log("entro");
+    //Validación de que el email este completo
+    alert(nombre + " " + apellido);
+   
+    if (nombre == "" || apellido == "" || email =="") {
+      const nombreError = document.getElementById("nombreError");
+      nombreError.removeAttribute("hidden");
+      //alert ("Todos los campos son obligatorios");
+        //return false;
+    } else {
+      //alert('Los datos ingresados son correctos');    
+      //return true;
+    }
+      //console.log(validateEmail("javier@g,ail.com"));
+    if(validateEmail())
+        {
+            alert('El mail esta correcto');                
+        }
+        else
+        {
+            alert('el mail no esta correcto');    
+        }
+      if(vehiculo.checked)
+      {
+          alert('Los datos ingresados son correctos');    
+          }
+          else
+          {
+              alert ('Debe seleccionar una opción en el campo Vehículo') ;  
+          }
+     
+   
+      return true;    
+}
 
-    if (nombre == "" || apellido == "" || email == "") {
-      alert ("Todos los campos son obligatorios");
-      //return false;
-} else {
-  //alert('Los datos ingresados son correctos');
-  //return true;
-
-  console.log(validateemail)
-  if(validateemail())
-{
-  alert('Los datos ingresados son correctos');
-}
-else
-{
-alert('el mail no esta correcto')
-}
-}
-if(vehiculo.checked)
-{
-  alert('Los datos ingresados son correctos');
-  }
-  else
-  {
-    alert('Debe seleccionar una ocpion en el campo Vehiculo');
-  }
-
-}
 
 
 function funcion1() {
@@ -57,3 +63,23 @@ function funcion1() {
   }
   
 
+
+  function funcion1() {
+    //Toma el valor del input con el id="num" y lo guarda en la variable x
+    let x = document.getElementById("num").value;
+    // chequea el valor ingesado
+    let text;
+    if (isNaN(x) || x < 1 || x > 10) // tengo tres condiciones con que una sea verdadera entra al if
+    {     // isNaN(x) pregunta si no es un nro
+      text = "Ingreso no valido";
+    }
+    else
+    {
+      text = "Ingreso valido";
+    }
+  
+    x = x+1
+    document.getElementById("demo").innerHTML = text;
+    document.getElementById("numero").innerHTML = x;
+  }
+  
